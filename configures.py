12ab17a94d31f0a63b71af3890b62f88d7142e01
1669454731@qq.com <1669454731@qq.com>
2022-12-16 00:00:23 +0800

    early_stopping

--- configures.py
+++ configures.py
@@ -6,21 +6,20 @@ from typing import List
 
 
 class DataParser(Tap):
-    dataset_dir: str = 'input.pkl'
+    dataset_dir: str = 'Data/input.pkl'
     dataset_name = 'code'
     num_node_features: int = 840
     num_classes: int = 3
     avg_num_nodes: int = 1000
-    data_split_ratio: List = [0.8, 0.1, 0.1]   # the ratio of training, validation and testing set for random split
-    batch_size: int = 64
+    data_split_ratio: List = [0.6, 0.2, 0.2]   # the ratio of training, validation and testing set for random split
+    batch_size: int = 10
     seed: int = 2
 
 
 class TrainParser(Tap):
-    learning_rate: float = 0.005
-    batch_size: int = 64
+    learning_rate: float = 0.0001
     weight_decay: float = 0.0
-    max_epochs: int = 800
+    max_epochs: int = 255
     save_epoch: int = 10
     early_stopping: int = 100
 
@@ -29,7 +28,7 @@ class ModelParser(Tap):
     device: str = "cpu"
     checkpoint: str = 'checkpoint'
     hidden_channels: int = 128
-    mlp_hidden: int = 64
+    mlp_hidden: int = 128
     model_name = 'dmon'
 
 
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from torch.optim import Adam
 import os
 import torch
 import shutil
+import torch.nn.functional as F
 
 
 def train():
@@ -17,6 +18,8 @@ def train():
 
     print('start training model==================')
     gnnNets = DMon(data_args, model_args)
+    gnnNets.to('cpu')
+
     criterion = nn.CrossEntropyLoss()
     optimizer = Adam(gnnNets.parameters(), lr=train_args.learning_rate, weight_decay=train_args.weight_decay)
 
@@ -37,9 +40,11 @@ def train():
         acc = []
         loss_list = []
         gnnNets.train()
+        loss_all = 0
 
         for data in dataloader['train']:
             data = data.to(model_args.device)
+
             pre, _ = gnnNets(data.x, data.edge_index, data.batch)
             loss = criterion(pre, data.y)
 
@@ -49,37 +54,73 @@ def train():
             torch.nn.utils.clip_grad_value_(gnnNets.parameters(), clip_value=2.0)
             optimizer.step()
 
+            # optimizer.zero_grad()
+            # pre, tot_loss = gnnNets(data.x, data.edge_index, data.batch)
+            # loss = F.nll_loss(pre, data.y.view(-1)) + tot_loss
+            # loss.backward()
+            # loss_all += data.y.size(0) * float(loss)
+            # optimizer.step()
+
             ## record
             _, prediction = torch.max(pre, -1)
-            loss_list.append(loss.item())
             acc.append(prediction.eq(data.y).cpu().numpy())
+            loss_list.append(loss.item())
 
         # report train msg
         epoch_acc = np.concatenate(acc, axis=0).mean()
         epoch_loss = np.average(loss_list)
         print(f"Train Epoch:{epoch}  |Loss: {epoch_loss:.3f} | Acc: {epoch_acc:.3f}")
 
+        # report eval msg
+        eval_state = evaluate_GC(dataloader['eval'], gnnNets, criterion)
+        print(f"Eval Epoch: {epoch} | Loss: {eval_state['loss']:.3f} | Acc: {eval_state['acc']:.3f}")
+
         # only save the best model
-        is_best = (epoch_acc > best_acc) or (epoch_loss < best_loss and epoch_acc >= best_acc)
-        if epoch_acc == best_acc:
+        is_best = (eval_state['acc'] > best_acc)
+
+        if eval_state['acc'] > best_acc:
+            early_stop_count = 0
+        else:
             early_stop_count += 1
+
         if early_stop_count > train_args.early_stopping:
             break
+
         if is_best:
-            if epoch_acc > best_acc:
-                best_acc = epoch_acc
-                early_stop_count = 0
-            if epoch_loss < best_loss:
-                best_loss = epoch_loss
+            best_acc = eval_state['acc']
+            early_stop_count = 0
         if is_best or epoch % train_args.save_epoch == 0:
-            save_best(ckpt_dir, epoch, gnnNets, model_args.model_name, epoch_acc, is_best)
+            save_best(ckpt_dir, epoch, gnnNets, model_args.model_name, eval_state['acc'], is_best)
 
     print(f"The best validation accuracy is {best_acc}.")
     # report test msg
     checkpoint = torch.load(os.path.join(ckpt_dir, f'{model_args.model_name}_best.pth'))
     gnnNets.update_state_dict(checkpoint['net'])
-    test_state, _ = test_GC(dataloader['train'], gnnNets, criterion)
+    test_state, _ = test_GC(dataloader['test'], gnnNets, criterion)
     print(f"Test: | Loss: {test_state['loss']:.3f} | Acc: {test_state['acc']:.3f}")
+
+
+def evaluate_GC(eval_dataloader, gnnNets, criterion):
+    acc = []
+    loss_list = []
+    gnnNets.eval()
+
+    with torch.no_grad():
+        for data in eval_dataloader:
+
+            data = data.to(model_args.device)
+            pre, _ = gnnNets(data.x, data.edge_index, data.batch)
+            loss = criterion(pre, data.y)
+
+            ## record
+            _, prediction = torch.max(pre, -1)
+            loss_list.append(loss.item())
+            acc.append(prediction.eq(data.y).cpu().numpy())
+
+        eval_state = {'loss': np.average(loss_list),
+                      'acc': np.concatenate(acc, axis=0).mean()}
+
+    return eval_state
 
 def test_GC(test_dataloader, gnnNets, criterion):
     acc = []
@@ -120,6 +161,7 @@ def save_best(ckpt_dir, epoch, gnnNets, model_name, eval_acc, is_best):
     torch.save(state, ckpt_path)
     if is_best:
         shutil.copy(ckpt_path, os.path.join(ckpt_dir, best_pth_name))
+    gnnNets.to('cpu')
 
 
 if __name__ == "__main__":
